00ec8b41d8d79d0949644eb8ce04a12c02fe98a8
Catlin Zilinski <cat@codecatz.com>
2018-09-09 19:15:58 -0700

    S4L127

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+ATank * ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto Tank = GetControlledTank();
+	if (Tank) {
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController BeginPlay: %s"), *Tank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController BeginPlay"));
+	}
+	
+}
+
+
---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+
+#include "Public/Tank.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	// Returns the Tank pawn that is being possessed.
+	ATank * GetControlledTank() const;
+
+	void BeginPlay() override;
+	
+	
+};
